0379bd181be81e3e227404c4574d0134461332bc
Steve <designerofthings@gmail.com>
2020-03-25 15:45:58 +0100

    Complete functions exercises

--- js_exercises.js
+++ js_exercises.js
@@ -135,6 +135,27 @@ function getNumber(prompt) {
 let left = getNumber('Enter the first number: ');
 let right = getNumber('Enter the second number: ');
 console.log(`${left} * ${right} = ${multiply(left, right)}`);
+
+
+ function times(number1, number2){
+   let result = number1 * number2;
+   console.log(result);
+   return result;
+ }
+ let one = times(1,1);
+ let two = times(2, one);
+ let three = times(3, two);
+ let four = times(4, three);
+ let five = times(5,four);
+
+
+function scream(words) {
+  words = words + '!!!!';
+  return;
+  console.log(words);
+}
+
+scream('Yipeee');
 */
 
- 
+
