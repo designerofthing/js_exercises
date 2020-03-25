91fad182878db470efaf9e8f8fb39d7900bbd02c
Steve <designerofthings@gmail.com>
2020-03-25 10:42:28 +0100

    exercises 1to10 the basics

--- js_exercises.js
+++ js_exercises.js
@@ -1,6 +1,8 @@
+// LAUNCHSCHOOL JS BOOK EXERCISES
+
 // var name = "John" + " Doe";
 // console.log(name);
-
+/*
 var number = 4936;
 
 lastNum = number % 10;
@@ -13,3 +15,26 @@ console.log((nextLastNum - lastNum) / 10);
 console.log((nextNum - nextLastNum) / 100);
 console.log((firstNum - nextNum) / 1000);
 
+Template literal
+> `5 plus 5 equals ${5 +5}`
+'5 plus 5 equals 10'
+> console.log(Number('5') + 10);
+15
+
+> console.log(parseInt('5') + 10);
+15
+
+> console.log(parseInt('5', 10) + 10);
+15
+
+> `The value of 5 + 10 is ${Number('5') + 10}`
+'The value of 5 + 10 is 15'
+> 
+*/
+
+names =["asta", "butterscotch", "pudding", "neptune", "darwin"];
+
+console.log(names);
+
+let pets = {asta: 'dog', butterscotch: 'cat', pudding: 'cat', neptune: 'fish', darwin: 'lizard'};
+console.log(pets);
